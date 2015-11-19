25c15e8ac5dc3a49d829644567f3009c84728b5b
Patricia Limcangco <plimcangco1@studentmail.nait.ca>
2015-11-19 15:34:30 -0700

    Seating Summary class

---
+++ eRestaurant Demo/eRestaurant.Framework/Entities/DTOs/SeatingSummary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Framework.Entities.DTOs
+{
+    public class SeatingSummary
+    {
+        public byte Table { get; set; } // the table number - Tables.TableNumber
+        public int Seating { get; set; } //seating capacity -Tables.Capacity
+        public bool Taken { get; set; } // calculated - true if occupied
+        public int? BillID { get; set; } //Bills.BillID (nullable)
+        public decimal? BillTotal { get; set; } //calculated - total bill (nullable)
+        public string Waiter { get; set; } //Waiter's name (nullable)
+        public string ReservationName { get; set; } //Reservations.ContactName (nullable)
+        
+
+    }
+}
